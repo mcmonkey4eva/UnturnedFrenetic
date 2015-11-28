5dc849621a60f7c5569446630fc15119ec817afe
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-11-28 02:59:32 -0800

    Initial commit: output the methods of the debug command

---
+++ UnturnedFreneticInjector/Injectables/CommandDebugInjectable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mono.Cecil;
+
+namespace UnturnedFreneticInjector.Injectables
+{
+    class CommandDebugInjectable : Injectable
+    {
+        public override void InjectInto(ModuleDefinition mdef)
+        {
+            TypeDefinition debugCMD = mdef.GetType("SDG.Unturned.CommandDebug");
+            foreach (MethodDefinition method in debugCMD.Methods)
+            {
+                Console.WriteLine("Found Debug method: " + method.Name);
+            }
+        }
+    }
+}
---
+++ UnturnedFreneticInjector/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using Mono.Cecil;
+
+namespace UnturnedFreneticInjector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reading file and loading assembly...");
+            AssemblyDefinition assemblyCSharpdll = AssemblyDefinition.ReadAssembly("Assembly-CSharp.dll");
+            ModuleDefinition mod = assemblyCSharpdll.MainModule;
+            Console.WriteLine("Loaded. Running all injectables...");
+            Assembly thisasm = Assembly.GetCallingAssembly();
+            foreach (Type type in thisasm.GetTypes())
+            {
+                if (!type.IsAbstract && type.BaseType == typeof(Injectable))
+                {
+                    Console.WriteLine("Running: " + type.Name);
+                    Injectable obj = (Injectable)Activator.CreateInstance(type);
+                    obj.InjectInto(mod);
+                    Console.WriteLine("Ran: " + type.Name);
+                }
+            }
+            Console.WriteLine("Ran all injectables. Saving...");
+            assemblyCSharpdll.Write("Assembly-CSharp.Patched.dll");
+            Console.WriteLine("Saved. Completing...");
+            Console.WriteLine("Completed.");
+        }
+    }
+}
