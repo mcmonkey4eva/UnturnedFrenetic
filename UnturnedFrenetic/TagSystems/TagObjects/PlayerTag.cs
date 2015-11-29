40c60641dd444a8ee84fff1c4a973782e21650e5
Morphan1 <morphanone@gmail.com>
2015-11-29 00:59:50 -0500

    Add basic player life tags
    Not really sure what vision is for

--- UnturnedFrenetic/TagSystems/TagObjects/PlayerTag.cs
+++ UnturnedFrenetic/TagSystems/TagObjects/PlayerTag.cs
@@ -65,6 +65,96 @@ namespace UnturnedFrenetic.TagSystems.TagObjects
                 // -->
                 case "is_pro":
                     return new TextTag(Internal.isPro).Handle(data.Shrink());
+                // <--[tag]
+                // @Name PlayerTag.health
+                // @Group Attributes
+                // @ReturnType TextTag
+                // @Returns the player's current health level. Maximum health level is 100.
+                // @Example "bob" .health returns "56".
+                // -->
+                case "health":
+                    return new TextTag(Internal.player.life.health).Handle(data.Shrink());
+                // <--[tag]
+                // @Name PlayerTag.food
+                // @Group Attributes
+                // @ReturnType TextTag
+                // @Returns the player's current food level. Maximum food level is 100.
+                // @Example "bob" .food returns "89".
+                // -->
+                case "food":
+                    return new TextTag(Internal.player.life.food).Handle(data.Shrink());
+                // <--[tag]
+                // @Name PlayerTag.water
+                // @Group Attributes
+                // @ReturnType TextTag
+                // @Returns the player's current water level. Maximum water level is 100.
+                // @Example "bob" .water returns "74".
+                // -->
+                case "water":
+                    return new TextTag(Internal.player.life.water).Handle(data.Shrink());
+                // <--[tag]
+                // @Name PlayerTag.virus
+                // @Group Attributes
+                // @ReturnType TextTag
+                // @Returns the player's current virus level. Maximum virus level is 100.
+                // @Example "bob" .virus returns "37".
+                // -->
+                case "virus":
+                    return new TextTag(Internal.player.life.virus).Handle(data.Shrink());
+                // <--[tag]
+                // @Name PlayerTag.stamina
+                // @Group Attributes
+                // @ReturnType TextTag
+                // @Returns the player's current stamina level. Maximum stamina level is 100.
+                // @Example "bob" .stamina returns "13".
+                // -->
+                case "stamina":
+                    return new TextTag(Internal.player.life.stamina).Handle(data.Shrink());
+                // <--[tag]
+                // @Name PlayerTag.oxygen
+                // @Group Attributes
+                // @ReturnType TextTag
+                // @Returns the player's current oxygen level. Maximum oxygen level is 100.
+                // @Example "bob" .oxygen returns "42".
+                // -->
+                case "oxygen":
+                    return new TextTag(Internal.player.life.oxygen).Handle(data.Shrink());
+                // <--[tag]
+                // @Name PlayerTag.vision
+                // @Group Attributes
+                // @ReturnType TextTag
+                // @Returns the player's current vision.
+                // @Example "bob" .vision returns "124".
+                // -->
+                case "vision": // TODO: find purpose, it seems to be more of a "blindness" level, as it starts from 0
+                    return new TextTag(Internal.player.life.vision).Handle(data.Shrink());
+                // <--[tag]
+                // @Name PlayerTag.is_bleeding
+                // @Group Attributes
+                // @ReturnType TextTag
+                // @Returns whether the player is bleeding.
+                // @Example "bob" .is_bleeding returns "false".
+                // -->
+                case "is_bleeding":
+                    return new TextTag(Internal.player.life.isBleeding).Handle(data.Shrink());
+                // <--[tag]
+                // @Name PlayerTag.is_broken
+                // @Group Attributes
+                // @ReturnType TextTag
+                // @Returns whether the player has a broken limb.
+                // @Example "bob" .is_broken returns "true".
+                // -->
+                case "is_broken":
+                    return new TextTag(Internal.player.life.isBroken).Handle(data.Shrink());
+                // <--[tag]
+                // @Name PlayerTag.is_freezing
+                // @Group Attributes
+                // @ReturnType TextTag
+                // @Returns whether the player is currently freezing.
+                // @Example "bob" .is_freezing returns "false".
+                // -->
+                case "is_freezing":
+                    return new TextTag(Internal.player.life.isFreezing).Handle(data.Shrink());
                 default:
                     return new TextTag(ToString()).Handle(data);
             }
