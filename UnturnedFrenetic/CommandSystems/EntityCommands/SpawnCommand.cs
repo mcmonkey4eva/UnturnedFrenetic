a6503cd19c74ffa42c8c01bc198eada2aabd0739
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2015-11-29 01:45:25 -0800

    Only transmit the relevant animal packet

--- UnturnedFrenetic/CommandSystems/EntityCommands/SpawnCommand.cs
+++ UnturnedFrenetic/CommandSystems/EntityCommands/SpawnCommand.cs
@@ -39,11 +39,19 @@ namespace UnturnedFrenetic.CommandSystems.EntityCommands
                 return;
             }
             AnimalManager.manager.addAnimal(asset.Internal.id, loc.ToVector3(), 0, false);
-            Animal anim = AnimalManager.animals[AnimalManager.animals.Count - 1];
+            Animal animal = AnimalManager.animals[AnimalManager.animals.Count - 1];
             foreach (SteamPlayer player in PlayerTool.getSteamPlayers())
             {
-                // TODO: Send only the relevant animal's packet, not all animals.
-                AnimalManager.manager.askAnimals(player.playerID.steamID);
+                AnimalManager.manager.channel.openWrite();
+                AnimalManager.manager.channel.write((ushort)AnimalManager.animals.Count);
+                AnimalManager.manager.channel.write(new object[]
+                {
+                    animal.id,
+                    animal.transform.position,
+                    MeasurementTool.angleToByte(animal.transform.rotation.eulerAngles.y),
+                    animal.isDead
+                });
+                AnimalManager.manager.channel.closeWrite("tellAnimals", player.playerID.steamID, ESteamPacket.UPDATE_RELIABLE_CHUNK_BUFFER);
             }
             // TODO: Maybe add the animal as a var to the current commandqueue.
             entry.Good("Successfully spawned a " + TagParser.Escape(asset.ToString()) + " at " + TagParser.Escape(loc.ToString()) + "!");
